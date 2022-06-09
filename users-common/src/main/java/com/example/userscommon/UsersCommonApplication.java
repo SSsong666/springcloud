2a0d84e1912138f22e428d534b843ba09ecbdc72
wangsong <13552367443@163.com>
2022-06-09 14:44:31 +0800

    加入user实体业务

---
+++ users-common/src/main/java/com/example/userscommon/UsersCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.userscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersCommonApplication.class, args);
+    }
+
+}
---
+++ users-common/src/test/java/com/example/userscommon/UsersCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userscommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
