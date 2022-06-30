75129ef5da15ff8fe2de13facf23d35fb3a54b06
wangsong <13552367443@163.com>
2022-06-30 10:19:45 +0800

    初始化user项目

---
+++ user-api/src/main/java/com/example/usersconsumer/UsersConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersConsumerApplication.class, args);
+    }
+
+}
---
+++ user-api/src/test/java/com/example/usersprovider/UsersProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
