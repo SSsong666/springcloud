8f538d8242ce3a2fc9aeab13f53da66c4ae57db4
wangsong <13552367443@163.com>
2022-06-30 13:18:42 +0800

    1.服务注册添加actuator上传自己的健康状态，手动更改服务状态 2.eureka客户端添加配置信息

---
+++ eureka-consumer/src/main/java/com/example/eurekaconsumer/HealthStatusController.java
@@ -0,0 +1,21 @@
+package com.example.eurekaconsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthStatusController {
+
+    @Autowired
+    HealthStatusService service;
+
+    @GetMapping("/health/status/{status}")
+    public String health(@PathVariable Boolean status) {
+
+        service.setStatus(status);
+        return service.getStatus();
+    }
+}
